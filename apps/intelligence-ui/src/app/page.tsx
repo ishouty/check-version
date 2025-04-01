4aa1d8c0f1c65b69bced72fb9b98ed80cf8a19ab
Andy Nguyen <andy.nguyen@sdx.com>
2025-04-01 20:41:53 +0100

    fix: rename

--- apps/intelligence-ui/src/app/page.tsx
+++ apps/intelligence-ui/src/app/page.tsx
@@ -1,14 +1,10 @@
-import SemanticForm from "./components/form/check-version/check-version.form.component";
+import CheckVersionForm from "./components/form/check-version/check-version.form.component";
 
 export default function Index() {
-  /*
-   * Replace the elements below with your own.
-   *
-   * Note: The corresponding styles are in the ./index.tailwind file.
-   */
+
   return (
     <div>
-      <SemanticForm />
+      <CheckVersionForm />
     </div>
   );
 }
